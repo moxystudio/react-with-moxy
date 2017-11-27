6816e0cac017d7d102de81c4d7d2a912732629bf
André Cruz <andremiguelcruz@msn.com>
2017-11-27 03:51:56 +0000

    chore: ensure env variables existence instead of setting default values

--- scripts/config/webpack/util/definePluginEnv.js
+++ scripts/config/webpack/util/definePluginEnv.js
@@ -1,9 +1,12 @@
-// Ensure default values on some mandatory environment variables
-process.env.NODE_ENV = process.env.NODE_ENV || 'production';
-process.env.PUBLIC_URL = (process.env.PUBLIC_URL || '').replace(/\/+$/, '');
-process.env.PUBLIC_ASSETS_URL = (process.env.PUBLIC_ASSETS_URL || '/build').replace(/\/+$/);
+const assert = require('assert');
+
+const validEnvironments = ['production', 'development', 'test'];
 
 function definePluginEnv() {
+    assert(validEnvironments.includes(process.env.NODE_ENV), `Expecting NODE_ENV to be one of: ${validEnvironments.join(', ')}`);
+    assert(process.env.PUBLIC_URL != null, 'Expecting PUBLIC_URL environment variable to be defined');
+    assert(process.env.PUBLIC_ASSETS_URL != null, 'Expecting PUBLIC_URL environment variable to be defined');
+
     // Grab NODE_ENV and RWM_* environment variables and prepare them to be
     // injected into the application via DefinePlugin in Webpack configuration
     const env = Object.keys(process.env)
@@ -18,9 +21,9 @@ function definePluginEnv() {
         NODE_ENV: process.env.NODE_ENV,
 
         // Useful to build full urls to be used in stuff like share URLs
-        PUBLIC_URL: process.env.PUBLIC_URL,
+        PUBLIC_URL: process.env.PUBLIC_URL.replace(/\/+$/, ''),
         // Useful for resolving the correct path to static assets in `public` folder
-        PUBLIC_ASSETS_URL: process.env.PUBLIC_ASSETS_URL,
+        PUBLIC_ASSETS_URL: process.env.PUBLIC_ASSETS_URL.replace(/\/+$/),
     });
 
     // Stringify all values so we can feed into Webpack DefinePlugin
